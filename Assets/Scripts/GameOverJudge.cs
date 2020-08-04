41b8f3406ea96077088f6722e36e9b9405559dc7
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-08-04 21:03:10 +0900

    スタート画面と、ゲームオーバー画面を追加。

---
+++ Assets/Scripts/GameOverJudge.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverJudge : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("Clash3");
+     
+        if(collision.gameObject.name=="Terrain"){
+     Debug.Log("Clash");
+            SceneManager.LoadScene("GameOverScene");
+            //音楽つける、リトライボタンを追加する。->ゲーム(スタート)画面に飛ぶ
+            
+          }
+       
+        
+    }
+
+// Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/Scripts/GoTitle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoTitle : MonoBehaviour
+{
+    public void OnClick(){
+     SceneManager.LoadScene ("Title");
+    }
+}
