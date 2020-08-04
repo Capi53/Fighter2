bb21c7e33b30fc3ec1c834b34001f154f8114294
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-08-04 21:38:13 +0900

    add OnTriggerBall.cs

---
+++ Assets/Scripts/OnTriggerBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class OnTriggerBall : MonoBehaviour {
+ 
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.CompareTag("Player")){
+            Debug.Log("aaaaaaaaa");
+            Destroy(gameObject);
+        }
+    }
+    private GameObject[] enemyObjects;
+
+	void Update () {
+
+		enemyObjects = GameObject.FindGameObjectsWithTag("Coin");
+		if(enemyObjects.Length == 0){
+			SceneManager.LoadScene("GameClear");
+		}
+	}
+}
